aa09ac8c7e5a536cb85f46c05c62a0e5c5b3c74e
Gabriel Reus <Gabriel.Reus@gmail.com>
2024-11-08 19:08:49 +0100

    Afegit el header amb colors

---
+++ includes/colors.h
@@ -0,0 +1,14 @@
+#ifndef COLORS_H
+# define COLORS_H
+
+#define COLOR_RST		"\033[0;39m"
+#define COLOR_GRAY		"\033[0;90m"
+#define COLOR_RED		"\033[0;91m"
+#define COLOR_GREEN		"\033[0;92m"
+#define COLOR_YELLOW	"\033[0;93m"
+#define COLOR_BLUE		"\033[0;94m"
+#define COLOR_MAGENTA	"\033[0;95m"
+#define COLOR_CYAN		"\033[0;96m"
+#define COLOR_WHITE		"\033[0;97m"
+
+#endif
